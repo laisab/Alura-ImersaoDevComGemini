a147d914f8b0da6bd4130269b08bf946aae4c459
Lais Aparecida Borges <86136694+laisab@users.noreply.github.com>
2024-09-06 17:03:08 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+//Base de dados da aplicação
+
+//Lista
+let dados = [
+    {titulo: "Rayssa Leal", 
+    descricao: "Nascida em Imperatriz, Maranhão, Rayssa Leal, a Fadinha, conquistou o mundo do skate com seu talento precoce e carisma contagiante. Aos poucos anos, ela já colecionava títulos e quebrava recordes, tornando-se a mais jovem medalhista olímpica brasileira da história. Com seu estilo inovador e manobras arriscadas, Rayssa inspira uma nova geração de skatistas, especialmente meninas. Além de sua carreira no skate, ela é conhecida por seu envolvimento em projetos sociais e por ser um exemplo de superação.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"},
+
+    {titulo: "Rebeca Andrade", 
+    descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e mundial. Bicampeã olímpica e multicampeã em diversas competições, Rebeca é a primeira brasileira a conquistar o ouro olímpico na ginástica artística e a maior medalhista da história do Brasil nos Jogos Olímpicos.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags: "ginasta ginastica ouro"}, 
+    
+    {titulo: "Beatriz Souza", 
+    descricao: "Beatriz Souza é uma judoca brasileira que vem conquistando o cenário internacional com suas impressionantes performances. Natural de Itariri, São Paulo, ela se destacou na categoria acima de 78 kg, demonstrando força, técnica e uma determinação ímpar.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judô judo judoka"}
+];
